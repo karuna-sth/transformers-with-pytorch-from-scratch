1380c8f76b5a0ab091a23bf9c0a180a756fbc03f
karuna-sth <karunashrestha824@gmail.com>
2024-03-17 23:45:53 +0545

    Updated

--- model.py
+++ model.py
@@ -72,3 +72,55 @@ class FeedForwardBlock(nn.Module):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
     
 
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h:int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0
+
+        self.d_k = d_model//h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # batch, h, seq_len, d_ ---> batch, h, seq_len, seq_len
+        attention_scores = (query @ key.transpsoe(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask==0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) # batch, h, seq_len, seq_len
+        if dropout is not None:
+            attention_scores  = dropout(attention_scores)
+        
+        return (attention_scores*value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        # Batch , seq_len, d_model --> batch, seq_len, h, d_k ---> Batch m h, seq_len, d_k
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = query.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = query.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_score = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        #batch, h, seq-LEN, SEQ_LEN --- > batch, seq_len, h, d_k ----> Batch, seq_len, d_model
+        x = x.transpose(1, 2).contiguous.view(x.shape[0], -1, self.h*self.d_k)
+
+        # batch, seq_len, d_model --- > batch, seq_len, d_model
+        return self.w_o*x
+    
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout()
